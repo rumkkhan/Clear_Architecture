386685181503651b250750701b19f3511af62e8b
Ramzan <rumkkhan@gmail.com>
2019-10-21 14:58:45 +0530

    Cqrs

---
+++ CleanArch.Infra.Bus/InMemoryBus.cs
@@ -0,0 +1,23 @@
+﻿using CleanArch.Domain.Core.Bus;
+using CleanArch.Domain.Core.Commands;
+using MediatR;
+using System;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Bus
+{
+    public sealed class InMemoryBus : IMediatorHandler
+    {
+        private readonly IMediator _mediator;
+
+        public InMemoryBus(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public Task SendCommand<T>(T command) where T : Command
+        {
+            return _mediator.Send(command);
+        }
+    }
+}
